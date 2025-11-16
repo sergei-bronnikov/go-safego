a964ae4f69daf4eff26fb3eeb624676e8f8b4784
Sergei Bronnikov <sergeibronnikov87@gmail.com>
2025-11-16 22:27:04 +0000

    with context

--- safego.go
+++ safego.go
@@ -1,33 +1,63 @@
 package safego
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 )
 
-func Go(fn func()) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("recovered from panic in goroutine:", r)
+func Go(fn func(), ctx ...context.Context) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println("recovered from panic in goroutine:", r)
+			}
+		}()
+		if len(ctx) > 0 {
+			c := ctx[0]
+			select {
+			case <-c.Done():
+				log.Println("goroutine cancelled:", c.Err())
+				return
+			default:
+				fn()
+			}
+		} else {
+			fn()
 		}
 	}()
-	fn()
 }
 
-func GoWithErrorHandler(fn func() error, errorHandler func(error)) {
-	defer func() {
-		if r := recover(); r != nil {
-			e := errors.New(fmt.Sprintf("recovered from panic in goroutine: %v", r))
-			errorHandler(e)
+func GoWithErrorHandler(fn func() error, errorHandler func(error), ctx ...context.Context) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				e := errors.New(fmt.Sprintf("recovered from panic in goroutine: %v", r))
+				errorHandler(e)
+			}
+		}()
+		if len(ctx) > 0 {
+			c := ctx[0]
+			select {
+			case <-c.Done():
+				e := errors.New(fmt.Sprintf("goroutine cancelled: %v", c.Err()))
+				errorHandler(e)
+				return
+			default:
+				if err := fn(); err != nil {
+					errorHandler(err)
+				}
+			}
+		} else {
+			if err := fn(); err != nil {
+				errorHandler(err)
+			}
 		}
 	}()
-	if err := fn(); err != nil {
-		errorHandler(err)
-	}
 }
 
-func ChanGo(fn func()) chan error {
+func ChanGo(fn func(), ctx ...context.Context) chan error {
 	errChan := make(chan error, 1)
 	go func() {
 		defer func() {
@@ -36,12 +66,23 @@ func ChanGo(fn func()) chan error {
 			}
 			close(errChan)
 		}()
-		fn()
+		if len(ctx) > 0 {
+			c := ctx[0]
+			select {
+			case <-c.Done():
+				errChan <- errors.New(fmt.Sprintf("goroutine cancelled: %v", c.Err()))
+				return
+			default:
+				fn()
+			}
+		} else {
+			fn()
+		}
 	}()
 	return errChan
 }
 
-func ChanGoWithError(fn func() error) chan error {
+func ChanGoWithError(fn func() error, ctx ...context.Context) chan error {
 	errChan := make(chan error, 1)
 	go func() {
 		defer func() {
@@ -50,8 +91,21 @@ func ChanGoWithError(fn func() error) chan error {
 			}
 			close(errChan)
 		}()
-		if err := fn(); err != nil {
-			errChan <- err
+		if len(ctx) > 0 {
+			c := ctx[0]
+			select {
+			case <-c.Done():
+				errChan <- errors.New(fmt.Sprintf("goroutine cancelled: %v", c.Err()))
+				return
+			default:
+				if err := fn(); err != nil {
+					errChan <- err
+				}
+			}
+		} else {
+			if err := fn(); err != nil {
+				errChan <- err
+			}
 		}
 	}()
 	return errChan
